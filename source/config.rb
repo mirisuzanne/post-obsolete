d1aef819e4f1fef5cb7895286e32f69c4da872cf
Eric Meyer <eric@oddbird.net>
2014-06-20 10:26:05 -0500

    Begin jinja2, add how-to

--- source/config.rb
+++ source/config.rb
@@ -6,11 +6,11 @@ require 'accoutrement'
 
 # Set this to the root of your project when deployed:
 http_path = "/"
-css_dir = "css"
+css_dir = "../css"
 sass_dir = "sass"
-images_dir = "images"
-javascripts_dir = "js"
-fonts_dir = "fonts"
+images_dir = "../images"
+javascripts_dir = "../js"
+fonts_dir = "../fonts"
 
 # You can select your preferred output style here (can be overridden via the command line):
 output_style = :expanded
@@ -20,10 +20,3 @@ relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
 line_comments = false
-
-
-# If you prefer the indented syntax, you might want to regenerate this
-# project again passing --syntax sass, or you can uncomment this:
-# preferred_syntax = :sass
-# and then run:
-# sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
